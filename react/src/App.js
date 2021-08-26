8d0797c06013b9b87cb0d5198a4ebe0bd9bd6505
Nyan Htet <nyanhtet@yahoo.com>
2021-08-25 21:33:49 -0700

    Update App.js

--- react/src/App.js
+++ react/src/App.js
@@ -1,20 +1,35 @@
-import './App.css';
-import React from "react";
+import "./App.css";
+import { React, useState } from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Home from "./pages/Home";
 import Pitch from "./pages/Pitch";
 import NoMatch from "./pages/NoMatch";
+import NavBar from "./Components/NavBar/NavBar";
+import NavBar2 from "./Components/NavBar2";
 
-function App() {
+// ******* this is working! ************
+//function App() {
+//   return (
+//     <Router>
+//       <NavBar />  <--- this is working fine
+//       <Switch>
+//         <Route exact path="/" component={Home} />
+//         <Route exact path="/pitches" component={Pitch} />
+//         <Route component={NoMatch} />
+//       </Switch>
+//     </Router>
+//   );
+// }
+
+const App = () => {
   return (
     <Router>
-        <Switch>
-          <Route exact path="/" component={Home} />
-          <Route exact path="/pitches" component={Pitch} />
-          <Route component={NoMatch} />
-        </Switch>
+      <Switch>
+        <Route path="/" component={Home} exact />
+        <Route path="/pitches" component={Pitch} exact />
+      </Switch>
     </Router>
   );
-}
+};
 
 export default App;
