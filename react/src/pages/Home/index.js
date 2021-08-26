d66a66c39a3d9af788d8940275f8f48abff59ecb
Nyan Htet <nyanhtet@yahoo.com>
2021-08-25 21:33:53 -0700

    Update index.js

--- react/src/pages/Home/index.js
+++ react/src/pages/Home/index.js
@@ -1,10 +1,29 @@
-import React from "react";
-import './style.css';
+import { React, useState } from "react";
+import "./style.css";
+import NavBar2 from "../../Components/NavBar2"; // not working
+import NavBar from "../../Components/NavBar/NavBar"; // working
 
-function Home() {
+// **** this is working ***
+// function Home() {
+//   return (
+//     <p>Homepage Yo</p>
+//   );
+// }
+
+// export default Home;
+
+const Home = () => {
+  const [isOpen, setOpen] = useState(false);
+
+  const toggle = () => {
+    setOpen(!isOpen);
+  };
+
   return (
-    <p>Homepage Yo</p>
+    <>
+      <NavBar2 toggle={toggle} />
+    </>
   );
-}
+};
 
 export default Home;
