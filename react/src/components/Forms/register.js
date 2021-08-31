67107075191e59efc0b86b663ac7c0bf5f1fd0e5
Nyan Htet <nyanhtet@yahoo.com>
2021-08-31 11:26:29 -0700

    added register form

---
+++ react/src/components/Forms/register.js
@@ -0,0 +1,42 @@
+import NavBar from "../NavBar";
+import { useState, React } from "react";
+import Sidebar from "../Sidebar";
+import {
+  MainContainer,
+  WelcomeText,
+  InputContainer,
+  Input,
+  ButtonContainer,
+  Button,
+  ForgotPassword,
+  LoginLink,
+} from "./formElements";
+
+const Register = () => {
+  const [isOpen, setOpen] = useState(false);
+
+  const toggle = () => {
+    setOpen(!isOpen);
+  };
+
+  return (
+    <>
+      <NavBar toggle={toggle} />
+      <Sidebar isOpen={isOpen} toggle={toggle} />
+      <MainContainer>
+        <WelcomeText>Sign Up</WelcomeText>
+        <InputContainer>
+          <Input type="text" placeholder="Full Name" />
+          <Input type="text" placeholder="Email address" />
+          <Input type="password" placeholder="Password" />
+        </InputContainer>
+        <ButtonContainer>
+          <Button>Sign Up for an account!</Button>
+        </ButtonContainer>
+        <LoginLink to="/login">Have an account? Click here to Login.</LoginLink>
+      </MainContainer>
+    </>
+  );
+};
+
+export default Register;
