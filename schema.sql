2f4e688b19591ac8642466eaed2a41e649b85d9a
Nyan Htet <nyanhtet@yahoo.com>
2021-10-12 17:23:29 -0700

    updated Schema

--- schema.sql
+++ schema.sql
@@ -8,8 +8,8 @@ CREATE DATABASE pitchin_db;
 CREATE TABLE "user" (
   username VARCHAR(25) PRIMARY KEY,
   password VARCHAR(25) NOT NULL,
-  first_name VARCHAR(25) NOT NULL,
-  last_name VARCHAR(25) NOT NULL,
+  firstName VARCHAR(25) NOT NULL,
+  lastName VARCHAR(25) NOT NULL,
   email VARCHAR(25) NOT NULL
     CHECK (position('@' IN email) > 1)
 );
