6476719227ea74a51c461cec70d87b446ac7b1fa
Nyan Htet <nyanhtet@yahoo.com>
2021-10-12 17:22:56 -0700

    added users routes

--- app.js
+++ app.js
@@ -2,7 +2,8 @@ const express = require("express");
 const app = express();
 const path = require("path");
 
-const pitchRoutes = require('./routes/pitch');
+const pitchRoutes = require("./routes/pitch");
+const usersRoutes = require("./routes/users");
 
 // CORS allows our backend to communicate with our frontend and vice versa.  Otherwise, we'll get an error in the browser every
 // time our frontend requests something from our backend, saying Cross Origin Resource Sharing not allowed.
@@ -13,10 +14,11 @@ if (process.env.NODE_ENV === "production") {
   app.use(express.static("react/build"));
 }
 
-app.use('/api/pitch', pitchRoutes);
+app.use("/api/pitch", pitchRoutes);
+app.use("api/users", usersRoutes);
 
 // If no API routes are hit, send the React app
-app.use(function(req, res) {
+app.use(function (req, res) {
   res.sendFile(path.join(__dirname, "react/build/index.html"));
 });
 
