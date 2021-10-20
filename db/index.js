944dbe66705d0852520debb6a30dea0e7eaf4809
Nyan Htet <nyanhtet@yahoo.com>
2021-10-19 17:22:21 -0700

    update config

--- db/index.js
+++ db/index.js
@@ -1,7 +1,7 @@
 /** Database setup for PitchIn. */
 
 const { Client } = require("pg");
-const { DB_URI } = require("./config");
+const { DB_URI } = require("../config/index");
 
 const db = new Client({
   connectionString: DB_URI,
