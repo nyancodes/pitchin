889791dab9a5d8e5ba931c45b4219d8d60bcc0ae
Nyan Htet <nyanhtet@yahoo.com>
2021-10-26 17:23:18 -0700

    Update index.js

--- react/src/components/FetchPitches/index.js
+++ react/src/components/FetchPitches/index.js
@@ -5,15 +5,28 @@ import React, { useState, useEffect } from "react";
 function FetchPitches() {
   const [pitches, setPitches] = useState([]);
 
-  useEffect(function fetchPitchesWhenMounted() {
-    async function fetchPitches() {
-      const pitchData = await axios.get("http://localhost:3001/pitches");
-      setPitches(pitchData.data);
-    }
+  useEffect(function () {
     fetchPitches();
   }, []);
 
-  return <div>{pitches ? <h2>{pitches}</h2> : <i>(loading)</i>}</div>;
+  async function fetchPitches() {
+    const pitchData = await axios.get("/pitch/all");
+    console.log(pitchData);
+    setPitches(pitchData.data);
+  }
+
+  return (
+    <div>
+      {pitches.map((pitch) => {
+        return (
+          <div key={pitch.username}>
+            <h2>{pitch.username}</h2>
+            <h1>{pitch.username}</h1>
+          </div>
+        );
+      })}
+    </div>
+  );
 }
 
 export default FetchPitches;
