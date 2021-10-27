a072b9cdac233078a1d52e715810e1b888c96d06
Nyan Htet <nyanhtet@yahoo.com>
2021-10-26 17:23:30 -0700

    Update app.js

--- app.js
+++ app.js
@@ -1,15 +1,13 @@
 const express = require("express");
-const path = require("path");
-const db = require("./db");
-
 const pitchRoutes = require("./routes/pitch");
 const usersRoutes = require("./routes/users");
 
 const app = express();
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 
 // CORS allows our backend to communicate with our frontend and vice versa.  Otherwise, we'll get an error in the browser every
 // time our frontend requests something from our backend, saying Cross Origin Resource Sharing not allowed.
-app.use(express.json());
 
 // Serve up static assets (usually on heroku)
 if (process.env.NODE_ENV === "production") {
