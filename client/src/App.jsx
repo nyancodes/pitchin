deb5036238045587663f88a9461eff2fe55cb80c
Nyan Htet <nyanhtet@yahoo.com>
2022-02-21 14:58:49 -0800

    added components and basic file structure

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,16 +1,19 @@
 import { useState } from 'react'
 import logo from './logo.svg'
 import './App.css'
+import {Navbar, Welcome, Footer, Services ,Transactions} from './components';
 
 function App() {
 
   return (
-    <div className="App">
-      <header className="App-header">
-        <h1 className="text-3xl font-bold underline">
-        Hello world!
-      </h1>
-      </header>
+    <div className="min-h-screen">
+      <div className="gradient-bg-welcome">
+        <Navbar />
+        <Welcome />
+      </div>
+      <Services />
+      <Transactions />
+      <Footer />
     </div>
   )
 }
