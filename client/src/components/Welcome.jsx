f11424494225d7dc457734d6322141e3cfe69859
Nyan Htet <nyanhtet@yahoo.com>
2022-02-21 22:08:27 -0800

    update welcome component

--- client/src/components/Welcome.jsx
+++ client/src/components/Welcome.jsx
@@ -1,7 +1,17 @@
+import { AiFillPlayCircle } from "react-icons/ai";
+import { SiEthereum } from "react-icons/si";
+import { BsInfoCircle } from "react-icons/bs";
+
 const Welcome = () => {
-    return (
-        <h1>Welcome</h1>
-    );
-}
+  return (
+    <div className="flex w-full justify-center items-center">
+      <div className="flex md:flex-row flex-col items-start justify-between md:p-20 py-12 px-4">
+        <div className="flex flex-1 justify-start flex-col md:mr-10">
+          <h2>PitchIn - Crowdfunding</h2>
+        </div>
+      </div>
+    </div>
+  );
+};
 
 export default Welcome;
