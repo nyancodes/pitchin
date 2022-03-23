9975cced675c4ab25e0acd7bf44f9042954a6ab1
Nyan Htet <nyanhtet@yahoo.com>
2022-03-22 21:11:18 -0700

    add transactions smart contract

---
+++ smart_contract/contracts/Transactions.sol
@@ -0,0 +1,56 @@
+// SPDX-License-Identifier: UNLICENSED
+
+pragma solidity ^0.8.0;
+
+contract Transactions {
+    uint256 transactionCount;
+
+    event Transfer(
+        address from,
+        address receiver,
+        uint256 amount,
+        string message,
+        uint256 timestamp,
+        string keyword
+    );
+
+    struct TrsnsferStruct {
+        address sender;
+        address receiver;
+        uint256 amount;
+        string message;
+        uint256 timestamp;
+        string keyword;
+    }
+
+    function addToBlockchain(
+        address payable receiver,
+        uint256 amount,
+        string memory message,
+        string memory keyword
+    ) public {
+        transactionCount += 1;
+        transactions.push(
+            TransferStruct(
+                msg.sender,
+                receiver,
+                amount,
+                message,
+                block.timestamp,
+                keyword
+            )
+        );
+        emit Transfer(
+            msg.sender,
+            receiver,
+            amount,
+            message,
+            block.timestamp,
+            keyword
+        );
+    }
+
+    function getAll() public view returns (TrsnsferStruct[] memory) {}
+
+    function getTransactionCount() public view returns (uint256) {}
+}
